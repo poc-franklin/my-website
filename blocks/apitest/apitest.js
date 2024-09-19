c2c6a50692b74298669f6c22c0261a8ad083d29a
Sachin Dey <sachin.dey@Indegene.com>
2024-09-19 12:57:53 +0530

    Update

--- blocks/apitest/apitest.js
+++ blocks/apitest/apitest.js
@@ -12,6 +12,13 @@ async function getApiCall(block) {
   const response = await fetch('https://localhost:8443/bin/readnode').then(res => res.clone().json())
   block.append(JSON.stringify(response));
   console.log('response');
+  const closeButton = document.createElement('button');
+  closeButton.classList.add('close-button');
+  closeButton.setAttribute('aria-label', 'Close');
+  closeButton.type = 'button';
+  closeButton.innerHTML = '<span class="icon icon-close"></span>';
+  closeButton.addEventListener('click', () => dialog.close());
+  dialog.prepend(closeButton);
   if (response.ok) {
     // Get JSON value from the response body
     return response.json();
