1081674857f476c519a225370b427b2d9d5eed59
Sachin Dey <sachin.dey@Indegene.com>
2024-09-19 12:45:12 +0530

    Update

--- blocks/apitest/apitest.js
+++ blocks/apitest/apitest.js
@@ -12,4 +12,9 @@ async function getApiCall(block) {
   const response = await fetch('https://localhost:8443/bin/readnode').then(res => res.clone().json())
   block.append(JSON.stringify(response));
   console.log('response');
+  if (response.ok) {
+    // Get JSON value from the response body
+    <div><h1>Welcome to ES6</h1></div>
+    return response.json();
+  }
 }
